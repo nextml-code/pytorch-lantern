5cb02d22ff1459874ff8654733ff704007dbf7a3
Richard <samedii@gmail.com>
2020-11-12 16:55:18 +0100

    refactor: remove template

--- workflow/template/{{cookiecutter.repository_name}}/{{cookiecutter.package_name}}/datastream/__init__.py
+++
@@ -1,8 +0,0 @@
-from {{cookiecutter.package_name}}.datastream.datasets import datasets
-from {{cookiecutter.package_name}}.datastream.augmenter import augmenter
-from {{cookiecutter.package_name}}.datastream.evaluate_datastreams import (
-    evaluate_datastreams
-)
-from {{cookiecutter.package_name}}.datastream.gradient_datastream import (
-    GradientDatastream
-)
