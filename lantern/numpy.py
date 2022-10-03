c736d07278095da184a57c901e7310ca49070a7d
Richard Löwenström <samedii@gmail.com>
2022-10-03 12:16:19 +0200

    improve: more features in types tensor and numpy

--- lantern/numpy.py
+++ lantern/numpy.py
@@ -1,34 +1,25 @@
 import numpy as np
-import io
-import cv2
+import torch
 
 
 class Numpy(np.ndarray):
-    @staticmethod
-    def from_matplotlib_figure(figure):
-        buffer = io.BytesIO()
-        figure.savefig(buffer, format="png", dpi=90, bbox_inches="tight")
-        buffer.seek(0)
-        image = np.frombuffer(buffer.getvalue(), dtype=np.uint8)
-        buffer.close()
-        image = cv2.imdecode(image, 1)
-        return image
-
     @classmethod
     def __get_validators__(cls):
         yield cls.validate
 
     @classmethod
-    def validate(cls, data):
+    def validate(cls, data) -> np.ndarray:
         if isinstance(data, cls):
             return data.view(np.ndarray)
         elif isinstance(data, np.ndarray):
             return data
+        elif isinstance(data, torch.Tensor):
+            return data.numpy()
         else:
             return np.array(data)
 
     @classmethod
-    def ndim(cls, ndim):
+    def ndim(cls, ndim) -> "Numpy":
         class InheritNumpy(cls):
             @classmethod
             def validate(cls, data):
@@ -40,7 +31,7 @@ class Numpy(np.ndarray):
         return InheritNumpy
 
     @classmethod
-    def dims(cls, dims):
+    def dims(cls, dims) -> "Numpy":
         class InheritNumpy(cls):
             @classmethod
             def validate(cls, data):
@@ -54,7 +45,7 @@ class Numpy(np.ndarray):
         return InheritNumpy
 
     @classmethod
-    def shape(cls, *sizes):
+    def shape(cls, *sizes) -> "Numpy":
         class InheritNumpy(cls):
             @classmethod
             def validate(cls, data):
@@ -67,24 +58,93 @@ class Numpy(np.ndarray):
         return InheritNumpy
 
     @classmethod
-    def between(cls, geq, leq):
+    def between(cls, ge, le) -> "Numpy":
         class InheritNumpy(cls):
             @classmethod
             def validate(cls, data):
                 data = super().validate(data)
-                data_min = data.min()
-                if data_min < geq:
-                    raise ValueError(f"Expected min value {geq}, got {data_min}")
 
-                data_max = data.min()
-                if data_max > leq:
-                    raise ValueError(f"Expected max value {leq}, got {data_max}")
+                if data.min() < ge:
+                    raise ValueError(
+                        f"Expected greater than or equal to {ge}, got {data.min()}"
+                    )
+
+                if data.max() > le:
+                    raise ValueError(
+                        f"Expected less than or equal to {le}, got {data.max()}"
+                    )
                 return data
 
         return InheritNumpy
 
     @classmethod
-    def dtype(cls, dtype):
+    def ge(cls, ge) -> "Numpy":
+        class InheritTensor(cls):
+            @classmethod
+            def validate(cls, data):
+                data = super().validate(data)
+                if data.min() < ge:
+                    raise ValueError(
+                        f"Expected greater than or equal to {ge}, got {data.min()}"
+                    )
+
+        return InheritTensor
+
+    @classmethod
+    def le(cls, le) -> "Numpy":
+        class InheritTensor(cls):
+            @classmethod
+            def validate(cls, data):
+                data = super().validate(data)
+
+                if data.max() > le:
+                    raise ValueError(
+                        f"Expected less than or equal to {le}, got {data.max()}"
+                    )
+                return data
+
+        return InheritTensor
+
+    @classmethod
+    def gt(cls, gt) -> "Numpy":
+        class InheritTensor(cls):
+            @classmethod
+            def validate(cls, data):
+                data = super().validate(data)
+
+                if data.min() <= gt:
+                    raise ValueError(f"Expected greater than {gt}, got {data.min()}")
+
+        return InheritTensor
+
+    @classmethod
+    def lt(cls, lt) -> "Numpy":
+        class InheritTensor(cls):
+            @classmethod
+            def validate(cls, data):
+                data = super().validate(data)
+
+                if data.max() >= lt:
+                    raise ValueError(f"Expected less than {lt}, got {data.max()}")
+                return data
+
+        return InheritTensor
+
+    @classmethod
+    def ne(cls, ne) -> "Numpy":
+        class InheritTensor(cls):
+            @classmethod
+            def validate(cls, data):
+                data = super().validate(data)
+
+                if (data == ne).any():
+                    raise ValueError(f"Unexpected value {ne}")
+                return data
+
+        return InheritTensor
+
+    @classmethod
+    def dtype(cls, dtype) -> "Numpy":
         class InheritNumpy(cls):
             @classmethod
             def validate(cls, data):
@@ -95,6 +155,66 @@ class Numpy(np.ndarray):
                 return new_data
 
         return InheritNumpy
+
+    @classmethod
+    def float(cls) -> "Numpy":
+        return cls.dtype(np.float32)
+
+    @classmethod
+    def float32(cls) -> "Numpy":
+        return cls.dtype(np.float32)
+
+    @classmethod
+    def half(cls) -> "Numpy":
+        return cls.dtype(np.float16)
+
+    @classmethod
+    def float16(cls):
+        return cls.dtype(np.float16)
+
+    @classmethod
+    def double(cls) -> "Numpy":
+        return cls.dtype(np.float64)
+
+    @classmethod
+    def float64(cls) -> "Numpy":
+        return cls.dtype(np.float64)
+
+    @classmethod
+    def int(cls) -> "Numpy":
+        return cls.dtype(np.int32)
+
+    @classmethod
+    def int32(cls) -> "Numpy":
+        return cls.dtype(np.int32)
+
+    @classmethod
+    def long(cls) -> "Numpy":
+        return cls.dtype(np.int64)
+
+    @classmethod
+    def int64(cls) -> "Numpy":
+        return cls.dtype(np.int64)
+
+    @classmethod
+    def short(cls) -> "Numpy":
+        return cls.dtype(np.int16)
+
+    @classmethod
+    def int16(cls) -> "Numpy":
+        return cls.dtype(np.int16)
+
+    @classmethod
+    def byte(cls) -> "Numpy":
+        return cls.dtype(np.uint8)
+
+    @classmethod
+    def uint8(cls) -> "Numpy":
+        return cls.dtype(np.uint8)
+
+    @classmethod
+    def bool(cls) -> "Numpy":
+        return cls.dtype(bool)
 
 
 def test_base_model():
@@ -143,9 +263,57 @@ def test_dtype():
     from pytest import raises
 
     class Test(BaseModel):
-        numbers: Numpy.dtype(np.uint8)
+        numbers: Numpy.uint8()
 
     Test(numbers=[1, 2, 3])
 
     with raises(ValueError):
         Test(numbers=[1.5, 2.2, 3.2])
+
+    class TestBool(BaseModel):
+        flags: Numpy.bool()
+
+    TestBool(flags=[True, False, True])
+
+    with raises(ValueError):
+        TestBool(numbers=[1.5, 2.2, 3.2])
+
+
+def test_from_torch():
+    import torch
+    from pydantic import BaseModel
+
+    class Test(BaseModel):
+        numbers: Numpy
+
+    numbers = torch.tensor([1, 2, 3])
+    numpy_numbers = Test(numbers=numbers).numbers
+
+    assert type(numpy_numbers) == np.ndarray
+    assert np.allclose(torch.from_numpy(numpy_numbers), numbers)
+
+
+def test_between():
+    from pydantic import BaseModel
+    from pytest import raises
+
+    class Test(BaseModel):
+        numbers: Numpy.between(1, 3.5)
+
+    Test(numbers=[1.5, 2.2, 3.2])
+
+    with raises(ValueError):
+        Test(numbers=[-1.5, 2.2, 3.2])
+
+
+def test_gt():
+    from pydantic import BaseModel
+    from pytest import raises
+
+    class Test(BaseModel):
+        numbers: Numpy.gt(1)
+
+    Test(numbers=[1.5, 2.2, 3.2])
+
+    with raises(ValueError):
+        Test(numbers=[1, 2.2, 3.2])
